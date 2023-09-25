853d0e9dc594036237359ae1005e6354651ee487
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-09-25 09:04:59 -0700

    refactore: update the script puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,14 +2,14 @@
 
 include stdlib
 
-file_line{ 'Declare identity file':
+file_line { 'Declare identity file':
   path    => '~/.ssh/config',
-  line    => '	IdentityFile ~/.ssh/school',
+  line    => '    IdentityFile ~/.ssh/school',
   replace => true,
 }
 
-file_line{ 'Turn off passwd auth':
+file_line { 'Turn off passwd auth':
   path    => '~/.ssh/config',
-  line    => '	PasswordAuthentification no',
+  line    => '    PasswordAuthentication no',
   replace => true,
 }
