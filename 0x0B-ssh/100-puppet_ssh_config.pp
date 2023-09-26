32bb55f10b05d9669eef4612695fad0f98a44ac5
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-09-26 01:10:21 -0700

    perf: increase a performance of puppet script to configure the ssh configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,13 +3,13 @@
 include stdlib
 
 file_line { 'Declare identity file':
-  path    => '~/.ssh/config',
+  path    => '/etc/ssh/ssh_config',
   line    => '    IdentityFile ~/.ssh/school',
   replace => true,
 }
 
 file_line { 'Turn off passwd auth':
-  path    => '~/.ssh/config',
+  path    => '/etc/ssh/ssh_config',
   line    => '    PasswordAuthentication no',
   replace => true,
 }
